9d9f1c1852db2e0298151b7defba5a4a3339a408
PiotrBerebecki <petebere@gmail.com>
2017-01-05 18:11:51 +0700

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -5,26 +5,37 @@ const socketIO = require('socket.io');
 
 
 const app = express();
+const publicPath = path.join(__dirname, '../public');
+app.use(express.static(publicPath));
+
 const server = http.createServer(app);
 const io = socketIO(server);
 
 
-const publicPath = path.join(__dirname, '../public');
-
-app.use(express.static(publicPath));
-
-
-// Event listeners for connecting and disconnecting server to client
 io.on('connection', (socket) => {
   console.log('New user connected');
   
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to chat app',
+    createdAt: new Date().getTime
+  });
+  
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime
+  });
+  
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+    
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
     });
+    
   });
   
   socket.on('disconnect', () => {
