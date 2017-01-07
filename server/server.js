996a5daffcef076d142fc4f28fe15b38c0a4279a
PiotrBerebecki <petebere@gmail.com>
2017-01-07 10:44:26 +0700

    Display welcome messages only in a give room

--- server/server.js
+++ server/server.js
@@ -18,14 +18,32 @@ const io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('New user connected');
   
-  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
-  
-  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
-  
   socket.on('join', (params, callback) => {
     if (!isRealString(params.name) || !isRealString(params.room)) {
       callback('Name and room name are required');
-    } 
+    }
+    
+    
+    // methods for emitting in socket.IO
+    // io.emit - emit to everyone
+    // socket.broadcast.emit - emit to everyone connect except for current user
+    // socket.emit - emit to one user
+    
+    // to target rooms you can chain the to() method
+    // io.to('React').emit
+    // socket.broadcast.to('React').emit
+    // no point to do: socket.to('React').emit as we are only targeting one user
+    
+    // leaving a room:
+    // socket.leave('React')
+    
+    socket.join(params.room);
+    
+    
+    
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+    socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined`));
+    callback();
   });
   
   socket.on('createMessage', (message, callback) => {
