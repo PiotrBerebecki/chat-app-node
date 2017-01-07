facfc1077d70d9c08ff1107fbeb6451d8c5c4119
PiotrBerebecki <petebere@gmail.com>
2017-01-07 10:22:19 +0700

    Add tests for isRealString validation

---
+++ server/utils/validation.test.js
@@ -0,0 +1,21 @@
+const expect = require('expect');
+
+const { isRealString } = require('./validation');
+
+
+describe('isRealString', () => {
+  
+  it('should reject non-string values', () => { 
+    expect(isRealString(98)).toBe(false);
+  });
+  
+  it('should reject strings with only spaces', () => { 
+    expect(isRealString('  ')).toBe(false);
+  });
+  
+  it('should allow strings with-non space characters', () => {
+    expect(isRealString(' Andy ')).toBe(true);
+    expect(isRealString('Andy')).toBe(true);
+  });
+  
+});
